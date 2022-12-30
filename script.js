160bbfb6b3bec36f21df05610f113b93714f5359
XeonBash <22185152+SubyiaLab@users.noreply.github.com>
2022-12-31 02:59:48 +0300

    Grid siz 16x16 working with reset button

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const mainContainer = document.querySelector(".divConatiner");
+const gridSizeBtn1 = document.querySelector(".gridSize16");
+const gridSizeBtn2 = document.querySelector(".gridSize64");
+const gridResetBtn = document.querySelector(".resetGrid");
+
+let coloroption1 = 'black';
+let coloroption2 = "white";
+let gridSize = 16;
+
+gridSizeBtn1.addEventListener('click', ()=> {
+    gridSize = 16;
+});
+
+gridSizeBtn2.addEventListener('click',()=>{
+    gridSize = 64;
+});
+
+
+
+const randomR = Math.floor(Math.random() * 256)
+const randomG = Math.floor(Math.random() * 256)
+const randomB = Math.floor(Math.random() * 256)
+let coloroption3 = `rgb(${randomR}, ${randomG}, ${randomB})`;
+
+for (let i=1; i<=gridSize*gridSize;i++){
+
+let divGrid = document.createElement("div");
+divGrid.className = "gridDiv"
+mainContainer.appendChild(divGrid);
+
+divGrid.addEventListener('mouseover', ()=>{
+    divGrid.style.background = coloroption1;
+});
+}
+
+
+const squares = document.querySelectorAll(".gridDiv");
+
+function resetDivs() {
+  squares.forEach((square) => {
+    square.style.background = "white";
+  });
+}
+
+gridResetBtn.addEventListener('click', ()=>{resetDivs();});
