90346895fba4372c646ce413acdc017d46e254a7
XeonBash <22185152+SubyiaLab@users.noreply.github.com>
2022-12-31 03:42:23 +0300

    Add style

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let gridSize = 16;
 
 gridSizeBtn1.addEventListener('click', ()=> {
     gridSize = 16;
+    
 });
 
 gridSizeBtn2.addEventListener('click',()=>{
@@ -23,14 +24,14 @@ const randomG = Math.floor(Math.random() * 256)
 const randomB = Math.floor(Math.random() * 256)
 let coloroption3 = `rgb(${randomR}, ${randomG}, ${randomB})`;
 
+
 for (let i=1; i<=gridSize*gridSize;i++){
 
 let divGrid = document.createElement("div");
 divGrid.className = "gridDiv"
 mainContainer.appendChild(divGrid);
-
 divGrid.addEventListener('mouseover', ()=>{
-    divGrid.style.background = coloroption1;
+divGrid.style.background = coloroption1;
 });
 }
 
